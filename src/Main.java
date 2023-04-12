87cbc0a2e4f1ffc79e1404dea5f29bde18ed5ecc
Majid <abmajidniaz@gmail.com>
2023-04-12 20:06:31 +0430

    ubdated

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Frame f1 = new Frame();
+        f1.setVisible(true);
+        f1.setLocation(300,200);
+        f1.setTitle("My First Application");
+        f1.setSize(300,200);
+        f1.setBackground(Color.lightGray);
+          }
+}
